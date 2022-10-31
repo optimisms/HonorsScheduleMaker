6235d39ca5a9278c5736567d1f355a4a431b5d18
optimisms <optimisms@users.noreply.github.com>
2022-10-30 20:29:26 -0600

    Slight refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,29 +8,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        if (args[1].equals("unsorted")) {
-            File studentList = new File(args[0]);
-            Scanner sc = new Scanner(studentList);
+        File studentList = new File(args[0]);
+        Scanner sc = new Scanner(studentList);
 
-            List<Student> classRoll = createStudentList(sc);
-            ScheduleMaker mySchedule = new ScheduleMaker();
-            mySchedule.createGroups(classRoll);
-
-            System.out.println(mySchedule);
-        }
+        ScheduleMaker mySchedule;
         if (args[1].equals("sorted")) {
-            File groupsList = new File(args[0]);
-            Scanner sc = new Scanner(groupsList);
-
             List<Student> group1 = createGroup(sc.nextLine());
             List<Student> group2 = createGroup(sc.nextLine());
             List<Student> group3 = createGroup(sc.nextLine());
             List<Student> group4 = createGroup(sc.nextLine());
 
-            ScheduleMaker mySchedule = new ScheduleMaker(group1, group2, group3, group4);
-
-            System.out.println(mySchedule);
+            mySchedule = new ScheduleMaker(group1, group2, group3, group4);
         }
+        else {// if (args[1].equals("unsorted")) {
+            mySchedule = new ScheduleMaker();
+
+            List<Student> classRoll = createStudentList(sc);
+            mySchedule.createGroups(classRoll);
+        }
+
+        System.out.println(mySchedule);
     }
 
     private static List<Student> createStudentList(Scanner sc) {
@@ -56,22 +53,6 @@ public class Main {
         }
         return students;
     }
-
-//    private static List<Student> parseNames(String nextLine) {
-//        Scanner sc = new Scanner(nextLine);
-//        sc.next();
-//        sc.next();
-//        List<Student> students = new ArrayList<>();
-//        while (sc.hasNext()) {
-//            String firstName = sc.next();
-//            String lastName = sc.next();
-//            if (lastName.contains(",")) {
-//                lastName = lastName.substring(0, lastName.length() - 1);
-//            }
-//            students.add(createStudent(firstName, lastName));
-//        }
-//        return students;
-//    }
 
     private static Student createStudent(String firstName, String lastName) {
         return new Student(firstName, lastName);
