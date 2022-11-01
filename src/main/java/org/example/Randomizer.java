ab350eb46ba16c82816d20db862cbfb9f1543896
optimisms <optimisms@users.noreply.github.com>
2022-11-01 13:31:47 -0600

    Moved operations related to randomization to a new static class

---
+++ src/main/java/org/example/Randomizer.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Random;
+
+public class Randomizer {
+    public static GroupNum newRandomGroupNum() {
+        int rand = getRandInt();
+
+        return GroupNum.parseInt(rand);
+    }
+
+    public static GroupNum newRandomGroupNum(GroupNum[] taken) {
+        int rand = getRandInt();
+
+        while (groupNumInArray(GroupNum.parseInt(rand), taken)) {
+            rand = getRandInt();
+        }
+        return GroupNum.parseInt(rand);
+    }
+
+    private static int getRandInt() { return new Random().nextInt(1, 5); }
+
+    private static boolean groupNumInArray(GroupNum rand, GroupNum[] taken) {
+        for (GroupNum i : taken) {
+            if (rand == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static GroupNum getLastGroupNum(GroupNum[] taken) {
+        if (taken.length != 3) { return null; }
+        if (groupNumInArray(GroupNum.ONE, taken)) {
+            if (groupNumInArray(GroupNum.TWO, taken)) {
+                if (groupNumInArray(GroupNum.THREE, taken)) {
+                    return GroupNum.FOUR;
+                } else { return GroupNum.THREE; }
+            } else { return GroupNum.TWO; }
+        } else { return GroupNum.ONE; }
+    }
+}
